528fbc8a13fea3e4a7cbeb8c2541c02fc44fa1c6
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-01-24 20:35:01 +0800

    Working on Handler

--- services/drive/handler.ts
+++ services/drive/handler.ts
@@ -1,6 +1,7 @@
 "use strict";
 
 const S3 = require("aws-sdk/clients/s3");
+const { randomUUID } = require("crypto");
 const DynamoDb = require("aws-sdk/clients/dynamodb");
 
 const s3 = new S3();
@@ -10,76 +11,120 @@ const S3_Bucket_Name = process.env.S3_BUCKET_NAME;
 const DYNAMO_DB_Table = process.env.DYNAMO_DB_Table;
 const DB_KEY = process.env.DB_KEY;
 
-export const sendForSuccess = (data) => {
+const SupportedFolder = ["images", "files", "videos"];
+const appKey = `${DB_KEY}#FILE`;
+
+export const respondToSuccess = (data) => {
   return {
     statusCode: 200,
     body: JSON.stringify(data),
   };
 };
-export const sendForError = (data) => {
+
+export const respondForError = (data) => {
   return {
     statusCode: 500,
     body: JSON.stringify({
-      message: JSON.stringify(data),
+      message: data,
     }),
   };
 };
 
-const SupportedFolder = ["images", "files", "videos"];
+export const isValidFolder = (folder: string): boolean => {
+  return SupportedFolder.includes(folder);
+};
 
-export const uploadToS3 = async (event, context) => {
+export const getFilesFromS3 = async (event) => {
+  const { folder } = event.pathParameters;
+  if (!isValidFolder(folder)) {
+    return respondForError("Not valid folder");
+  }
+  return respondToSuccess({
+    message: "Go Serverless v3.0! Your function executed successfully!",
+    S3_Bucket_Name,
+    DYNAMO_DB_Table,
+    DB_KEY,
+    uid: randomUUID(),
+  });
+};
+
+export const uploadToS3 = async (event) => {
   const { folder, file } = event.pathParameters;
+  if (!isValidFolder(folder)) {
+    return respondForError("Not valid folder");
+  }
   //get the image data from upload
   const buffer = Buffer.from(
     event.body.replace(/^data:image\/\w+;base64,/, ""),
     "base64"
   );
+  const ContentType = event.headers["Content-Type"];
   const params = {
     Bucket: S3_Bucket_Name,
     Key: `${folder}/${file}`,
     Body: buffer,
-    ContentType: event.headers["Content-Type"],
+    ContentType,
   };
 
   try {
     const result = await s3.putObject(params).promise();
-    console.log(result);
-    return sendForSuccess({
-      message: "this is uploaded to S3",
+    await dynamoDB
+      .putItem({
+        TableName: DYNAMO_DB_Table,
+        Item: {
+          appKey: appKey,
+          sortKey: `file#${file}`,
+          createdDate: new Date(),
+          updatedDate: new Date(),
+          path: folder,
+          type: ContentType,
+          label: file,
+          name: file,
+        },
+      })
+      .promise();
+    return respondToSuccess({
       folder,
       file,
+      ...result,
     });
   } catch (error) {
-    return sendForError(error.message);
+    return respondForError(error.message);
   }
 };
 
-export const deleteFromS3 = async (event, context) => {
-  return sendForSuccess({
-    message: "this is delete from S3",
-  });
-};
-
-export const getFilesFromS3 = async (event) => {
-  await dynamoDB
-    .putItem({
-      TableName: DYNAMO_DB_Table,
-      Item: {
-        appKey: { S: "APP#KM#FILES" },
-        sortKey: { S: "file#test" },
-      },
+export const deleteFromS3 = async (event) => {
+  const { folder, file } = event.pathParameters;
+  if (!isValidFolder(folder)) {
+    return respondForError("Not valid folder");
+  }
+  const result = await s3
+    .deleteObject({
+      Bucket: S3_Bucket_Name,
+      Key: `${folder}/${file}`,
     })
     .promise();
-  return sendForSuccess({
-    message: "Go Serverless v3.0! Your function executed successfully!",
-    S3_Bucket_Name,
-    DYNAMO_DB_Table,
-    DB_KEY
+
+  var params = {
+    TableName: DYNAMO_DB_Table,
+    Key: {
+      appKey: appKey,
+      sortKey: `file#${file}`,
+    },
+  };
+  await dynamoDB.deleteItem(params).promise();
+  return respondToSuccess({
+    message: "this is delete from S3",
+    ...result,
   });
 };
 
-export const updateS3File = async () => {
-  return sendForSuccess({
+export const updateS3File = async (event) => {
+  const { folder, file } = event.pathParameters;
+  if (!isValidFolder(folder)) {
+    return respondForError("Not valid folder");
+  }
+  return respondToSuccess({
     message: "Updated the file metadata from S3",
   });
 };
--- services/types.d.ts
+++ services/types.d.ts
@@ -5,3 +5,11 @@ export interface IBaseDB {
   createdDate?: string;
   updatedDate?: string;
 }
+
+export interface IFile extends IBaseDB {
+  name: string;
+  metadata: Record<string, string>;
+  label: string;
+  path: string;
+  type: string;
+}
