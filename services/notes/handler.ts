34fa677ed6315e0628e8c811998ac1c816b992b9
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-01-25 15:26:31 +0800

    Notes get an item

--- services/notes/handler.ts
+++ services/notes/handler.ts
@@ -3,11 +3,10 @@ const { randomUUID } = require("crypto");
 
 const dynamoDB = new DynamoDb();
 
-const S3_Bucket_Name = process.env.S3_BUCKET_NAME;
 const DYNAMO_DB_Table = process.env.DYNAMO_DB_Table;
 const DB_KEY = process.env.DB_KEY;
 
-const appKey = `${DB_KEY}#FILE`;
+const appKey = `${DB_KEY}#NOTE`;
 
 const transformResult = (items) => {
   return items.map((item) => {
@@ -58,10 +57,41 @@ export const getNotes = async () => {
   return respondToSuccess({ notes: transformResult(result.Items) });
 };
 
+export const addNote = async (event) => {
+  const note = JSON.parse(event.body);
+  const uid = randomUUID();
+  const dbResult = await dynamoDB
+    .putItem({
+      TableName: DYNAMO_DB_Table,
+      Item: {
+        appKey: { S: appKey },
+        sortKey: { S: `note#${uid}` },
+        createdDate: { S: new Date().toISOString() },
+        updatedDate: { S: new Date().toISOString() },
+        id: { S: uid },
+        title: { S: note.title || "" },
+        content: { S: note.content || "" },
+      },
+    })
+    .promise();
+  return respondToSuccess({
+    id: uid,
+    dbResult,
+  });
+};
+
 export const getNote = async (event) => {
   const { id } = event.pathParameters;
+  const params = {
+    TableName: DYNAMO_DB_Table,
+    Key: {
+      appKey: { S: appKey },
+      sortKey: { S: `note#${id}` },
+    },
+  };
+  const result = await dynamoDB.getItem(params).promise();
 
-  return respondToSuccess({ message: "this is vimal menon" });
+  return respondToSuccess({ message: "this is vimal menon", result });
 };
 export const createFolder = async () => {
   return null;
